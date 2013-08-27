2c88472c854af085d15a85c1028bcf5f6d82bfe4
OLX - Raul <dev@develop45.(none)>
2013-08-27 08:57:09 -0300

    practica1

---
+++ Practica1/src/primero/FiguraGeometrica.java
@@ -0,0 +1,18 @@
+package primero;
+
+public abstract class FiguraGeometrica {
+	
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public abstract void dibujar();
+	
+
+}
