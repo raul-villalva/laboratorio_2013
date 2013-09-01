08d6bf015d52115a76cbd048036b87e1137b728b
raul-villalva <raul.e.villalva@gmail.com>
2013-09-01 18:27:50 -0300

    practica1-singleton

---
+++ Practica1/src/primero/TestSingleton.java
@@ -0,0 +1,13 @@
+package primero;
+
+public class TestSingleton {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.CrearSingleton();
+
+	}
+
+}
